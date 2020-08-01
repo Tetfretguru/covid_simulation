55ace82a9318b85f822d2ed8c52e2fecf16e53fb
Martin Ghiena <josemartinghiena@gmail.com>
2020-08-01 14:45:12 -0300

    Layouts de las gráficas terminadas

--- plottings.py
+++ plottings.py
@@ -5,15 +5,18 @@ import stats as st
 
 def grafica_diaria(x_dias, y_casos):
     """ A modo de prueba, se usa modelo determinista"""   
-    
-    recuperados(x_dias,y_casos)
-    plt.scatter(x_dias, y_recuperados, label='recovered')
+    y_fiambres = st.muertos(x_dias, y_casos)
+    y_recovered = st.recuperados(x_dias,y_casos)
     
     plt.title('COVID-19 outbreak daily simulation')
     plt.xlabel('Days simulated')
     plt.ylabel('Daily new cases')
 
-    plt.plot(x_dias, y_casos, label='new cases')
+    plt.plot(x_dias, y_casos, label='New cases')
+    plt.plot(x_dias, y_recovered, label='Recovered')
+    plt.plot(x_dias, y_fiambres, 'r-', label='Deaths')
+    
+    plt.legend(loc='best')
     grafica_diarios = plt.show()
 
     return grafica_diarios
@@ -22,16 +25,18 @@ def grafica_total(x_dias, y_casos):
     y_acumulados = st.acumulados(x_dias, y_casos)
     y_casos = st.recuperados(x_dias, y_casos)
     y_rec_acumulados = st.acumulados(x_dias, y_casos)
-   
+    y_muertos = st.muertos(x_dias, y_casos)
+    
     plt.title('COVID-19 outbreak simulation')
     plt.xlabel('Days simulated')
     plt.ylabel('Total cases')
 
     plt.plot(x_dias, y_acumulados, label='new cases')
-    plt.scatter(x_dias, y_rec_acumulados, label='new cases')
+    plt.plot(x_dias, y_rec_acumulados, label='recovered')
+    plt.plot(x_dias, y_muertos, 'r-', label='deaths')
+    plt.legend(loc='best')
     grafica_acumulados = plt.show()
+    
     
     return grafica_acumulados
 
-def grafica_total_muertes(x_dias, y_casos):
-    pass
