a18b7df4151f31d43776bfbee92e11050ad83977
Martin Ghiena <josemartinghiena@gmail.com>
2020-08-01 17:08:54 -0300

    Modulo prototipo de alta de paises creado y mejorado el sistmea de main simulation

---
+++ paises.py
@@ -0,0 +1,10 @@
+# Clase para crear objetos como países
+class Country:
+    def __init__(self, name, continent, population, active_cases):
+        self.name = name
+        self.continent = continent
+        self.population = population
+        self.active_cases = active_cases
+    
+
+
